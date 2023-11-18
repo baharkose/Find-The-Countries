4b993b0ac59bc09a13311f9597e802d4454d844c
baharkose <baharkse17@gmail.com>
2023-11-18 14:44:08 +0300

    fixed li element displaying

--- app.js
+++ app.js
@@ -1,145 +1,141 @@
-// https://restcountries.com/v3.1/all
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.querySelector(".search-input");
+    const searchBtn = document.querySelector(".search-button");
+    const resultTable = document.querySelector(".result-table");
+    let suggestionList = document.getElementById("suggesstion-list");
 
-const searchInput = document.querySelector(".search-input")
-const searchBtn = document.querySelector(".search-button")
-const resultTable = document.querySelector(".result-table")
-const suggesstionList = document.getElementById("suggesstion-list")
+    let allCountries = [];
 
-
-// FETCHING 
-
-const fetchCountry = async () => {
-    try{
-        const response = await fetch("https://restcountries.com/v3.1/all")
-        if(!response.ok){
-           throw new Error(`sth went wrong ${response.status}`) 
-        }
-        const data = await response.json()
-        console.log(data);
-        showCountry(data)
-    }
-    catch (err){
-        console.log(err);
-    }
-}
-
-
-// SHOW COUNTRIES
-
-const showCountry = (countryData) => {
-    countryData.forEach((country)=>{
-
-        const {flags, name, region, capital, languages, borders , continents, currencies, maps, population} = country
-
-        // console.log(flags, name, region, capital,);
-        let countryName = name.common.toLowerCase();
-        console.log(countryName);
-        // INPUT PROCESS
-
-        searchInput.addEventListener("input", function(event) {
-            //  Assign suggestions with filtered
-            const suggesstions = getFilteredSuggesstions(event.target.value, countryData)
-            // display filtred suggestions
-            displaySuggestions(suggesstions);
-
-        })   
-        searchBtn.addEventListener("click", () =>{
-            const searchInputA = searchInput.value.toLowerCase().trim();
-
-            console.log("clicked");
-            console.log(searchInput.value);
-            console.log(countryName);
-
-            if(searchInputA == countryName){
-                        resultTable.innerHTML = `
-                        <tr>
-                        <th colspan="2" class=""resim>
-                            <img src="${flags.png}"/>
-                        </th>
-                        </tr>
-                            <tr>
-                                <th colspan="2" class="h2 text-center">${name.common}</th>
-                            </tr>
-                            <tr>
-                                <th><i class="fa-solid fa-earth-americas"></i> Region:</th>
-                                <td>${region}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><i class="fa-solid fa-landmark-flag"></i> Capitals:</th>
-                                <td>${capital}</td>
-
-                            </tr>
-                            <tr>
-                                <th scope="row"><i class="fa-solid fa-comment-dots"></i> Languages:</th>
-                                <td colspan="2">${Object.keys(languages)}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row"> <i class="fa-solid fa-coins"></i> Currencies:</th>
-                                <td colspan="2">${Object.keys(currencies)}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><i class="fa-solid fa-users-line"></i> Population:</th>
-                                <td colspan="2">${population}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><i class="fa-solid fa-road-barrier"></i> Borders:</th>
-                                <td colspan="2" class="brdr text-truncate">${borders}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><i class="fa-solid fa-location-dot"></i> Maps:</th>
-                                <td colspan="2">
-                                <a href="${maps.googleMaps}" target="_blank">Go to the google map <a/>
-
-                                </td>
-                            </tr>
-
-                `
-
+    const fetchCountry = async () => {
+        try {
+            const response = await fetch("https://restcountries.com/v3.1/all");
+            if (!response.ok) {
+                throw new Error(`Something went wrong: ${response.status}`);
             }
+            const data = await response.json();
+            allCountries = data;
+            showCountry("Turkey");
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-            // searchEvent(countryName)
-        })   
-    
-    })
-}
+    const showCountry = (targetCountryName) => {
+        resultTable.innerHTML = "";
+
+        const targetCountry = allCountries.find(country => country.name.common === targetCountryName);
+
+        if (targetCountry) {
+            const { flags, name, region, capital, languages, borders, currencies, maps, population } = targetCountry;
+
+            resultTable.innerHTML += `
+                     <tr>
+            <th colspan="2" class=""resim>
+                <img src="${flags.png}"/>
+            </th>
+            </tr>
+                <tr>
+                    <th colspan="2" class="h2 text-center">${name.common}</th>
+                </tr>
+                <tr>
+                    <th><i class="fa-solid fa-earth-americas"></i> Region:</th>
+                    <td>${region}</td>
+                </tr>
+                <tr>
+                    <th scope="row"><i class="fa-solid fa-landmark-flag"></i> Capitals:</th>
+                    <td>${capital}</td>
+
+                </tr>
+                <tr>
+                    <th scope="row"><i class="fa-solid fa-comment-dots"></i> Languages:</th>
+                    <td colspan="2">${Object.keys(languages)}</td>
+                </tr>
+                <tr>
+                    <th scope="row"> <i class="fa-solid fa-coins"></i> Currencies:</th>
+                    <td colspan="2">${Object.keys(currencies)}</td>
+                </tr>
+                <tr>
+                    <th scope="row"><i class="fa-solid fa-users-line"></i> Population:</th>
+                    <td colspan="2">${population}</td>
+                </tr>
+                <tr>
+                    <th scope="row"><i class="fa-solid fa-road-barrier"></i> Borders:</th>
+                    <td colspan="2" class="brdr text-truncate">${borders}</td>
+                </tr>
+                <tr>
+                    <th scope="row"><i class="fa-solid fa-location-dot"></i> Maps:</th>
+                    <td colspan="2">
+                    <a href="${maps.googleMaps}" target="_blank">Go to the google map <a/>
+
+                    </td>
+                </tr>
 
 
+            `;
+        }
+    };
 
-window.onload = () =>{
-   fetchCountry() 
-   searchInput.focus()
-}
-
-
-
-
-
-//  filtered suggessions
-
-function getFilteredSuggesstions(searchInput, countryData) {
-    return countryData.filter(country => country.name.common.toLowerCase().includes(searchInput));
-
-}
-
-
-// displaying
-
-function displaySuggestions(suggestions){
-    suggesstionList.innerHTML = "";
-    suggestions.forEach((suggestion) =>{
-        const li = document.createElement("li");
-        li.textContent = suggestion.name.common;
-        li.addEventListener("click",()=>{
-            searchInput.value = suggestion.name.common;
-            suggesstionList.innerHTML = "";
+    const findCountry = (inputCountryName) => {
+        const filteredCountries = allCountries.filter(country => {
+            const countryName = country.name.common.toLowerCase();
+            return countryName.startsWith(inputCountryName);
         });
-        suggesstionList.appendChild(li);
-    })
-}
 
+        showCountry(filteredCountries.length > 0 ? filteredCountries[0].name.common : "");
+    };
 
+    searchInput.addEventListener("input", () => {
+        const inputCountryName = searchInput.value.trim().toLowerCase();
+        findCountry(inputCountryName);
+    });
 
+    searchBtn.addEventListener("click", () => {
+        const inputCountryName = searchInput.value.trim().toLowerCase();
+        findCountry(inputCountryName);
+    });
 
+    // filtered suggestions
+    function getFilteredSuggestions(searchInput, allCountries) {
+        return allCountries.filter(country => country.name.common.toLowerCase().includes(searchInput));
+    }
 
+    // displaying suggestions
+    function displaySuggestions(suggestions) {
+        console.log("Displaying suggestions:", suggestions);
 
+    if (suggestionList) {
+        suggestionList.style.display="block";
+        // Clear previous suggestions
+        suggestionList.innerHTML = "";
 
+        // Create and append new li elements
+        suggestions.forEach((suggestion) => {
+            const li = document.createElement("li");
+            li.textContent = suggestion.name.common;
+
+            // Add a click event listener to set the input value and clear the suggestion list
+            li.addEventListener("click", (event) => {
+                searchInput.value = event.target.textContent;
+                suggestionList.style.display = "none";
+                showCountry(searchInput.value)
+            });
+
+            suggestionList.appendChild(li);
+        });
+
+        // Remove the "hidden" class to show the suggestion list
+      
+    }
+    }
+
+    searchInput.addEventListener("input", function (event) {
+        console.log("Input changed:", event.target.value);
+
+        // Assign suggestions with filtered
+        let suggestions = getFilteredSuggestions(event.target.value, allCountries);
+        // display filtered suggestions
+        displaySuggestions(suggestions);
+    });
+
+    fetchCountry();
+});
--- yedek.js
+++ yedek.js
@@ -1,101 +1,145 @@
-const searchInput = document.querySelector(".search-input");
-const searchBtn = document.querySelector(".search-button");
-const resultTable = document.querySelector(".result-table");
-const suggesstionList = document.getElementById("suggesstion-list");
+// https://restcountries.com/v3.1/all
 
-let allCountries = [];  // Initialize as an empty string
+const searchInput = document.querySelector(".search-input")
+const searchBtn = document.querySelector(".search-button")
+const resultTable = document.querySelector(".result-table")
+const suggesstionList = document.getElementById("suggesstion-list")
+
+
+// FETCHING 
 
 const fetchCountry = async () => {
-    try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
-        if (!response.ok) {
-            throw new Error(`sth went wrong ${response.status}`);
+    try{
+        const response = await fetch("https://restcountries.com/v3.1/all")
+        if(!response.ok){
+           throw new Error(`sth went wrong ${response.status}`) 
         }
-        const data = await response.json();
-        allCountries = data;
-        showCountry(data, "Turkey");
-    } catch (err) {
+        const data = await response.json()
+        console.log(data);
+        showCountry(data)
+    }
+    catch (err){
         console.log(err);
     }
-};
+}
+
+
+// SHOW COUNTRIES
+
+const showCountry = (countryData) => {
+    countryData.forEach((country)=>{
+
+        const {flags, name, region, capital, languages, borders , continents, currencies, maps, population} = country
+
+        // console.log(flags, name, region, capital,);
+        let countryName = name.common.toLowerCase();
+        console.log(countryName);
+        // INPUT PROCESS
+
+        searchInput.addEventListener("input", function(event) {
+            //  Assign suggestions with filtered
+            const suggesstions = getFilteredSuggesstions(event.target.value, countryData)
+            // display filtred suggestions
+            displaySuggestions(suggesstions);
+
+        })   
+        searchBtn.addEventListener("click", () =>{
+            const searchInputA = searchInput.value.toLowerCase().trim();
+
+            console.log("clicked");
+            console.log(searchInput.value);
+            console.log(countryName);
+
+            if(searchInputA == countryName){
+                        resultTable.innerHTML = `
+                        <tr>
+                        <th colspan="2" class=""resim>
+                            <img src="${flags.png}"/>
+                        </th>
+                        </tr>
+                            <tr>
+                                <th colspan="2" class="h2 text-center">${name.common}</th>
+                            </tr>
+                            <tr>
+                                <th><i class="fa-solid fa-earth-americas"></i> Region:</th>
+                                <td>${region}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><i class="fa-solid fa-landmark-flag"></i> Capitals:</th>
+                                <td>${capital}</td>
+
+                            </tr>
+                            <tr>
+                                <th scope="row"><i class="fa-solid fa-comment-dots"></i> Languages:</th>
+                                <td colspan="2">${Object.keys(languages)}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row"> <i class="fa-solid fa-coins"></i> Currencies:</th>
+                                <td colspan="2">${Object.keys(currencies)}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><i class="fa-solid fa-users-line"></i> Population:</th>
+                                <td colspan="2">${population}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><i class="fa-solid fa-road-barrier"></i> Borders:</th>
+                                <td colspan="2" class="brdr text-truncate">${borders}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><i class="fa-solid fa-location-dot"></i> Maps:</th>
+                                <td colspan="2">
+                                <a href="${maps.googleMaps}" target="_blank">Go to the google map <a/>
+
+                                </td>
+                            </tr>
+
+                `
+
+            }
+
+            // searchEvent(countryName)
+        })   
+    
+    })
+}
 
 
 
-const showCountry = (countries) => {
-
-    resultTable.innerHTML = "";
-
-    countries.forEach((country) => {
-        const { flags, name, region, capital, languages, borders, continents, currencies, maps, population } = country;
-
-        resultTable.innerHTML += `
-            <!-- Your HTML for displaying country information -->
-            <tr>
-                <th colspan="2" class="resim">
-                    <img src="${flags.png}"/>
-                </th>
-            </tr>
-            <tr>
-                <th colspan="2" class="h2 text-center">${name.common}</th>
-            </tr>
-            <tr>
-                <th><i class="fa-solid fa-earth-americas"></i> Region:</th>
-                <td>${region}</td>
-            </tr>
-            <tr>
-            <th scope="row"><i class="fa-solid fa-landmark-flag"></i> Capitals:</th>
-            <td>${capital}</td>
-            </tr>
-            <tr>
-                <th scope="row"><i class="fa-solid fa-comment-dots"></i> Languages:</th>
-                <td colspan="2">${Object.keys(languages)}</td>
-            </tr>
-            <tr>
-                <th scope="row"> <i class="fa-solid fa-coins"></i> Currencies:</th>
-                <td colspan="2">${Object.keys(currencies)}</td>
-            </tr>
-            <tr>
-                <th scope="row"><i class="fa-solid fa-users-line"></i> Population:</th>
-                <td colspan="2">${population}</td>
-            </tr>
-            <tr>
-                <th scope="row"><i class="fa-solid fa-road-barrier"></i> Borders:</th>
-                <td colspan="2" class="brdr text-truncate">${borders}</td>
-            </tr>
-            <tr>
-                <th scope="row"><i class="fa-solid fa-location-dot"></i> Maps:</th>
-                <td colspan="2">
-                <a href="${maps.googleMaps}" target="_blank">Go to the google map <a/>
-
-                </td>
-            </tr>
+window.onload = () =>{
+   fetchCountry() 
+   searchInput.focus()
+}
 
 
-        `;
-    });
-};
-
-const findCountry = (inputCountryName) => {
-    const filteredCountries = allCountries.filter((country) => {
-        const countryName = country.name.common.toLowerCase();
-        return countryName.startsWith(inputCountryName);
-    });
-
-    showCountry(filteredCountries);
-};
-
-searchInput.addEventListener('input', () => {
-    const inputCountryName = searchInput.value.trim().toLowerCase();
-    findCountry(inputCountryName);
-});
 
 
-searchBtn.addEventListener('click', () => {
-    const inputCountryName = searchInput.value.trim().toLowerCase();
-    findCountry(inputCountryName);
-})
 
-window.addEventListener("load", () => {
-    fetchCountry();
-  })
-fetchCountry();
+//  filtered suggessions
+
+function getFilteredSuggesstions(searchInput, countryData) {
+    return countryData.filter(country => country.name.common.toLowerCase().includes(searchInput));
+
+}
+
+
+// displaying
+
+function displaySuggestions(suggestions){
+    suggesstionList.innerHTML = "";
+    suggestions.forEach((suggestion) =>{
+        const li = document.createElement("li");
+        li.textContent = suggestion.name.common;
+        li.addEventListener("click",()=>{
+            searchInput.value = suggestion.name.common;
+            suggesstionList.innerHTML = "";
+        });
+        suggesstionList.appendChild(li);
+    })
+}
+
+
+
+
+
+
+
